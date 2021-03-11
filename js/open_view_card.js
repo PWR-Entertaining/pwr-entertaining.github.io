5aa3e369776283839bcc8744f1685094284afef2
Vlad Artyomkin <artyomkinvlad@yandex.by>
2021-03-11 15:36:41 +0300

    fix js

--- js/open_view_card.js
+++ js/open_view_card.js
@@ -5,7 +5,7 @@ var partList = {
     4: "pc_part"
 }
 
-var elem = function () {
+var openCard = function () {
     var parentEl = this.parentNode;
     for(var i = 1; i<=4; i++){
         for(var j = 5; j<=15; j += 5){
@@ -15,8 +15,7 @@ var elem = function () {
     changeCard.call(this);
     this.setAttribute("style", "margin-left:0px; z-index: 11;");
     // this.classList.remove('playstation_part_hover', 'xbox_part_hover', 'nintendo_part_hover', 'pc_part_hover');//убирает движение "главного дива" при наведении
-    this.setAttribute("onclick", 'location.href="page_game.html"'); //назначает onclick переход на страницу с игрой
-    // this.setAttribute("onmouseup", 'mouseLeave.call(this.parentNode)');
+    this.setAttribute("onclick", 'next.call(this)'); //назначает onclick переход на страницу с игрой
 }
 
 var changeCard = function (){
@@ -31,7 +30,7 @@ var changeCard = function (){
         if(parentEl.children[i] != this){ //просматриваем все карточки кроме нажатой
             parentEl.children[i].classList.add(`game_card_move_left_${countMove}px`) //добавляем им новые классы
             // parentEl.children[i].classList.remove(partList[i])//убираем обычные класс
-            parentEl.children[i].setAttribute("onclick", 'elem.call(this)');//возвращает нажатие на div "part" ссылка на метод
+            parentEl.children[i].setAttribute("onclick", 'openCard.call(this)');//возвращает нажатие на div "part" ссылка на метод
             countMove += 5;
         }
     }
@@ -43,8 +42,22 @@ var mouseLeave = function (){
     for(var i = 1; i<=4; i++){
         this.children[i].removeAttribute("style");
         this.children[i].removeAttribute("onmouseup");
-        this.children[i].setAttribute("onclick", 'elem.call(this)');
+        this.children[i].setAttribute("onclick", 'openCard.call(this)');
         this.children[i].classList.add(`${partList[i]}_hover`);
         this.children[i].classList.remove(`game_card_move_left_5px`, `game_card_move_left_10px`, `game_card_move_left_15px`);
     }
+}
+
+var next = function(){
+    var parentEl = this.parentNode;
+    parentEl.children[0].classList.remove('game_link_logo_only_display_none');
+    
+    for(var i = 1; i<=4; i++){
+        parentEl.children[i].removeAttribute("style");
+        parentEl.children[i].removeAttribute("onmouseup");
+        parentEl.children[i].setAttribute("onclick", 'openCard.call(this)');
+        parentEl.children[i].classList.add(`${partList[i]}_hover`);
+        parentEl.children[i].classList.remove(`game_card_move_left_5px`, `game_card_move_left_10px`, `game_card_move_left_15px`);
+    }
+    location.href = "page_game.html";
 }
