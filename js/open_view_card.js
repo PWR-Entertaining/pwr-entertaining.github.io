fbf5aafa0608c6a42404a39f0842b68aceeef214
Vlad Artyomkin <artyomkinvlad@yandex.by>
2021-03-15 11:54:48 +0300

    fix

--- js/open_view_card.js
+++ js/open_view_card.js
@@ -14,7 +14,7 @@ var openCard = function () {
     }
     changeCard.call(this);
     this.setAttribute("style", "margin-left:0px; z-index: 11;");
-    this.setAttribute("onclick", 'next.call(this)'); //назначает onclick переход на страницу с игрой
+    this.setAttribute("onclick", 'openGamePage.call(this)'); //назначает onclick переход на страницу с игрой
 }
 
 var changeCard = function (){
@@ -35,7 +35,17 @@ var changeCard = function (){
 
 var mouseLeave = function (){
     this.children[0].classList.remove('game_link_logo_only_display_none');
-    
+    removeCertainAttributes.call(this)
+}
+
+var openGamePage = function(){
+    var parentEl = this.parentNode;
+    parentEl.children[0].classList.remove('game_link_logo_only_display_none');
+    removeCertainAttributes.call(parentEl)
+    location.href = "page_game.html";
+}
+
+ var removeCertainAttributes = function(){
     for(var i = 1; i<=4; i++){
         this.children[i].removeAttribute("style");
         this.children[i].removeAttribute("onmouseup");
@@ -43,18 +53,4 @@ var mouseLeave = function (){
         this.children[i].classList.add(`${partList[i]}_hover`);
         this.children[i].classList.remove(`game_card_move_left_5px`, `game_card_move_left_10px`, `game_card_move_left_15px`);
     }
-}
-
-var next = function(){
-    var parentEl = this.parentNode;
-    parentEl.children[0].classList.remove('game_link_logo_only_display_none');
-    
-    for(var i = 1; i<=4; i++){
-        parentEl.children[i].removeAttribute("style");
-        parentEl.children[i].removeAttribute("onmouseup");
-        parentEl.children[i].setAttribute("onclick", 'openCard.call(this)');
-        parentEl.children[i].classList.add(`${partList[i]}_hover`);
-        parentEl.children[i].classList.remove(`game_card_move_left_5px`, `game_card_move_left_10px`, `game_card_move_left_15px`);
-    }
-    location.href = "page_game.html";
-}
+ }
